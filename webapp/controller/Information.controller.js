c4a72aee23c06c6051cc8ff5ea1381aa217fabe0
Mohamad Mujtaba Ahmed <mujtaba@mohamads-mbp.speedport.ip>
2022-12-27 22:57:59 +0100

    set model for fotter

--- webapp/controller/Information.controller.js
+++ webapp/controller/Information.controller.js
@@ -1,12 +1,24 @@
 sap.ui.define([
 	"./BaseController",
-	"sap/ui/core/UIComponent"
-], function(BaseController, UIComponent) {
+	"sap/ui/model/json/JSONModel"
+], function (BaseController, JSONModel) {
 	"use strict";
 
 	return BaseController.extend("ag.agasown.controller.Information", {
 		onInit: function () {
-			this.setHeaderModel();
+			var oViewModel = new JSONModel({
+				welcomeLogo: "ag/agasown/img/LOGO-HQ.png",
+				welcomeLogoVertical: "ag/agasown/img/Vertical-HQ.png",
+				facebook: "ag/agasown/img/facebook.svg",
+				youtube: "ag/agasown/img/youtube.svg",
+				instagram: "ag/agasown/img/instagram.svg",
+				pinterest: "ag/agasown/img/pinterest.svg",
+				ShoppingBags: "ag/agasown/img/ShoppingBags.jpg",
+				ShoppingCart: "ag/agasown/img/ShoppingCart.jpg",
+				AboutUs: "ag/agasown/img/AboutUsPage.jpg",
+				Jewel: "ag/agasown/img/goyna.jpg",
+			});
+			this.getView().setModel(oViewModel, "view");
 		}
 	});
 });
