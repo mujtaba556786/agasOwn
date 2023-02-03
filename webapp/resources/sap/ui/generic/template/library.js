3fd20aead2b90a564c535dd4a885076fec0a7fd2
Mohamad Mujtaba Ahmed <mujtaba@mohamads-mbp.speedport.ip>
2023-02-03 12:02:00 +0100

    url index

--- webapp/resources/sap/ui/generic/template/library.js
+++
@@ -1,7 +0,0 @@
-/*!
- * SAPUI5
-
-(c) Copyright 2009-2020 SAP SE. All rights reserved
- */
-sap.ui.define(["sap/ui/core/Core","sap/ui/core/library","sap/ui/comp/library","sap/ui/generic/app/library"],function(e){"use strict";var r=e.initLibrary({name:"sap.ui.generic.template",version:"1.109.0",dependencies:["sap.ui.core","sap.ui.comp","sap.ui.generic.app"],types:[],interfaces:[],controls:[],elements:[],noLibraryCSS:true});return r},true);
-//# sourceMappingURL=library.js.map
--- webapp/resources/sap/ui/mdc/ui/Container.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=Container.js.map
